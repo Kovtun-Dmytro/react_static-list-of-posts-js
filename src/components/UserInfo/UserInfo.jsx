df81ce4d634f20b1c8777f6cee941c522f8624da
Dmytro Kovtun <kovtundima2910@gmail.com>
2025-06-24 09:47:37 +0300

    i add solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,8 @@
-export const UserInfo = () => <>Put the user here</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => (
+  <div className="user-info">
+    <p>{user.name}</p>
+    <p>{user.email}</p>
+  </div>
+);
